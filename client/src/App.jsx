3821f95f1d7f1df3838486631c2e095da7e1a257
alicemac93 <alice.maciejewska@gmail.com>
2022-01-28 07:28:56 -0600

    feat: add fundamental components
    Add index and app with routes and product data

---
+++ client/src/App.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import ProductDetail from './containers/ProductDetail';
+import Footer from './Footer';
+import Header from './Header';
+import {
+    Routes,
+    Route
+  } from "react-router-dom";
+import { useQuery } from '@apollo/client';
+import { GET_PRODUCT } from './graphql/queries/getProduct';
+
+
+const App = () => {
+    const { data, loading, error } = useQuery(GET_PRODUCT);
+
+    if (loading) {
+        return <h1>The data is loading..</h1>
+    }
+    if (error) return <p>There has been an error..</p>
+
+    const { product } = data;
+
+    return (
+        <div className="app-container">
+            <Header />
+            <main className="main-content">
+            <Routes>
+                <Route
+                    path={`productDetail/${product.id}`}
+                    element={
+                        <ProductDetail
+                            product={product}
+                            id={product.id} />
+                    }
+                />
+            </Routes>
+            </main>
+            <Footer />
+    </div>
+    )
+};
+
+export default App;
